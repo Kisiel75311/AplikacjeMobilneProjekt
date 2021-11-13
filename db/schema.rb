0744123ae279da8ded3c4f4ad746e6e65cc9f633
Kisiel75311 <szymon.kisiela@gmail.com>
2021-11-13 15:23:34 +0100

    powrot

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,64 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_12_155138) do
+ActiveRecord::Schema.define(version: 2021_11_12_172817) do
 
-  create_table "students", force: :cascade do |t|
-    t.string "index"
-    t.string "name"
+  create_table "exercises", force: :cascade do |t|
+    t.string "description"
+    t.string "type_of_exercise"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "favourites", force: :cascade do |t|
+    t.date "date"
+    t.integer "user_id", null: false
+    t.integer "plan_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["plan_id"], name: "index_favourites_on_plan_id"
+    t.index ["user_id"], name: "index_favourites_on_user_id"
+  end
+
+  create_table "plans", force: :cascade do |t|
+    t.string "plan_name"
+    t.string "published"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_plans_on_user_id"
+  end
+
+  create_table "plans_exercises", id: false, force: :cascade do |t|
+    t.integer "plan_id"
+    t.integer "exercise_id"
+    t.index ["exercise_id"], name: "index_plans_exercises_on_exercise_id"
+    t.index ["plan_id"], name: "index_plans_exercises_on_plan_id"
+  end
+
+  create_table "trainings", force: :cascade do |t|
+    t.date "date"
+    t.float "status"
+    t.integer "user_id", null: false
+    t.integer "plan_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["plan_id"], name: "index_trainings_on_plan_id"
+    t.index ["user_id"], name: "index_trainings_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "password"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "favourites", "plans"
+  add_foreign_key "favourites", "users"
+  add_foreign_key "plans", "users"
+  add_foreign_key "trainings", "plans"
+  add_foreign_key "trainings", "users"
 end
